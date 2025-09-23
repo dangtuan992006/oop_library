9002f457c9832f2693c5646eee6b5d220de7ed25
unknown <tuandangngoc5@gmail.com>
2025-09-23 09:00:29 +0700

    lop co ban

---
+++ library.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+// lớp đối tượng 
+class book{
+    int bid; // book id
+    String bname; // book name
+    String genre; // thể loại
+    int quantity; //số lượng
+    int cost; // giá sách
+}
+class customer{
+    int cusid; // id customer
+    String cusname; // customer mame
+}
+
+
+//main
+public class library{
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
