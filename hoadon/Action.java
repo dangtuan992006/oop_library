89897e392013d9d7cf7f18848b5e7a01e79251d3
nhatvinh161006-gif <nhatvinh161006@gmail.com>
2025-10-24 00:41:36 +0700

    Create Action interface for invoice management

---
+++ hoadon/Action.java
@@ -0,0 +1,11 @@
+package QuanLyHoaDon;
+import QuanLyNhaSach.QuanLyNhaSach;
+public interface action2 {
+    void them(QuanLyNhaSach quanLyNhaSach);
+    void sua();
+    void xoa();
+    void timkiem();
+    void hiendanhsach();
+    void DocFile();
+    void ghidulieu();
+}
